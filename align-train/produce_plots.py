ad54c12f49ec447643e83d6c4343400c50cb370b
Yaroslav Nechaev <remper@me.com>
2018-01-30 17:33:27 +0100

    new subproject for training-related python code
    for now includes just the plot producer script for the results of the Evaluate pipeline

---
+++ align-train/produce_plots.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+from math import ceil, sqrt
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import argparse
+import re
+
+class Sample:
+    def __init__(self, line):
+        line = line.split('\t')
+        self.p = float(line[1])
+        self.r = float(line[2])
+        self.f1 = float(line[3])
+        self.t1 = float(line[4])
+        self.t2 = float(line[5])
+
+def main():
+    parser = argparse.ArgumentParser(description='Plots precision/recall curves')
+    parser.add_argument('--input', metavar='#', nargs='+', help='files from evaluation pipelines')
+    parser.add_argument('--output', metavar='#', help='an output plot file')
+    args = parser.parse_args()
+
+    # Parsing files
+    files = dict()
+    plots = 0
+    min_prec = 1.0
+    for file in args.input:
+        files[file] = dict()
+        cur_section = None
+        cur_subsection = None
+        with open(file, 'r') as reader:
+            for line in reader:
+                line = line.rstrip()
+                if ':' in line:
+                    cur_section = line.rstrip(':')
+                    cur_subsection = 'All'
+                    files[file][cur_section] = dict()
+                    continue
+                if '\t' not in line:
+                    cur_subsection = line
+                    continue
+
+                sample = Sample(line)
+                if sample.p < min_prec:
+                    min_prec = sample.p
+                subsection_name = "%s %.1f min improvement" % (cur_subsection, sample.t1)
+                if subsection_name not in files[file][cur_section]:
+                    plots += 1
+                    files[file][cur_section][subsection_name] = []
+                files[file][cur_section][subsection_name].append(sample)
+
+    # Producing joint graphs
+    file1 = files[args.input[0]]
+    plots //= len(files)
+    grid_size = int(ceil(sqrt(plots)))
+    saturation = int(ceil(plots / grid_size))
+    print("Plots: %d Grid size: %d Saturation: %d" % (plots, grid_size, saturation))
+    fig = plt.figure(figsize=(7*grid_size, 7*saturation))
+    plot = 0
+    for section in file1:
+        for subsection in file1[section]:
+            plot += 1
+            sp = fig.add_subplot(grid_size, grid_size, plot)
+            sp.title.set_text("%s %s" % (section, subsection))
+
+            sp.set_xlabel("Recall")
+            sp.set_ylabel("Precision")
+
+            sp.set_xlim(left=0, right=1)
+            sp.set_ylim(bottom=min_prec, top=1)
+
+            for file in files:
+                label = re.match('(.+)\.[a-z]+$', file).group(1)
+                x = []
+                y = []
+                source = sorted(files[file][section][subsection], key=lambda sample : sample.r)
+                for sample in source:
+                    x.append(sample.r)
+                    y.append(sample.p)
+                sp.plot(x, y, '-', label=label)
+            sp.legend(numpoints=1, loc='best')
+    fig.tight_layout()
+    fig.savefig(args.output)
+
+if __name__ == '__main__':
+    main()
