d4acc2aa2083f0ee5d47d77a7c18132212239b46
Yaroslav Nechaev <remper@me.com>
2018-03-20 19:22:49 +0100

    Flink version bump and filtering based on a closed set of users

---
+++ align/src/main/java/eu/fbk/fm/alignments/utils/flink/GzipTextOutputFormat.java
@@ -0,0 +1,68 @@
+package eu.fbk.fm.alignments.utils.flink;
+
+import org.apache.flink.api.java.io.TextOutputFormat;
+import org.apache.flink.core.fs.FSDataOutputStream;
+import org.apache.flink.core.fs.Path;
+
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+public class GzipTextOutputFormat<T> extends TextOutputFormat<T> {
+
+    public GzipTextOutputFormat(Path outputPath) {
+        super(outputPath);
+    }
+
+    public GzipTextOutputFormat(Path outputPath, String charset) {
+        super(outputPath, charset);
+    }
+
+    @Override
+    public void open(int taskNumber, int numTasks) throws IOException {
+        super.open(taskNumber, numTasks);
+
+        this.stream = new GZIPWrappedStream(this.stream, 4096, false);
+    }
+
+    public static class GZIPWrappedStream extends FSDataOutputStream {
+
+        private GZIPOutputStream stream;
+        private FSDataOutputStream original;
+
+        public GZIPWrappedStream(FSDataOutputStream original, int size, boolean syncFlush) throws IOException {
+            this.stream = new GZIPOutputStream(original, size, syncFlush);
+            this.original = original;
+        }
+
+        @Override
+        public void write(final int b) throws IOException {
+            this.stream.write(b);
+        }
+
+        @Override
+        public void write(final byte[] b, final int off, final int len) throws IOException {
+            this.stream.write(b, off, len);
+        }
+
+        @Override
+        public void close() throws IOException {
+            this.stream.close();
+        }
+
+
+        @Override
+        public void flush() throws IOException {
+            this.stream.flush();
+        }
+
+        @Override
+        public void sync() throws IOException {
+            this.original.sync();
+        }
+
+        @Override
+        public long getPos() throws IOException {
+            return this.original.getPos();
+        }
+    }
+}
---
+++ align/src/main/java/eu/fbk/fm/profiling/FilterUserData.java
@@ -0,0 +1,155 @@
+package eu.fbk.fm.profiling;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import eu.fbk.fm.alignments.index.BuildUserLSA;
+import eu.fbk.fm.alignments.utils.flink.GzipTextOutputFormat;
+import eu.fbk.fm.alignments.utils.flink.JsonObjectProcessor;
+import eu.fbk.utils.core.CommandLine;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Processes the stream and filters out everything that is unrelated to the list of users
+ */
+public class FilterUserData {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BuildUserLSA.class);
+
+    private static final String RESULTS_PATH = "results-path";
+    private static final String LIST_PATH = "list";
+    private static final String TWEETS_PATH = "tweets-path";
+
+    private String[] uids;
+
+    public FilterUserData(String[] uids) {
+        this.uids = uids;
+    }
+
+    public void startPipeline(String inputPath, String outputPath) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        final Configuration parameters = new Configuration();
+        parameters.setBoolean("recursive.file.enumeration", true);
+
+        env
+            .readTextFile(inputPath).withParameters(parameters)
+            .flatMap(new Deserializer(uids))
+            .output(new GzipTextOutputFormat<>(new Path(outputPath)));
+
+        env.execute();
+    }
+
+    public static final class Deserializer implements FlatMapFunction<String, String>, JsonObjectProcessor {
+
+        private static final long serialVersionUID = 1L;
+        private static final Gson GSON = new Gson();
+
+        private String[] uids;
+
+        public Deserializer(String[] uids) {
+            this.uids = uids;
+        }
+
+        public boolean checkIfShouldPassThrough(JsonObject object) {
+            JsonObject entities = object.getAsJsonObject("entities");
+            JsonObject userObject = object.getAsJsonObject("user");
+
+            if (entities == null || userObject == null) {
+                return false;
+            }
+
+            //Check if the author is in the list
+            final String username = get(userObject, String.class, "screen_name");
+            if (Arrays.stream(uids).anyMatch(username::equalsIgnoreCase)) {
+                return true;
+            }
+
+            //Check if one of the mentions is in the list
+            JsonArray rawMentions = entities.getAsJsonArray("user_mentions");
+            for (JsonElement rawMention : rawMentions) {
+                String mentionName = get(rawMention.getAsJsonObject(), String.class, "screen_name");
+                if (Arrays.stream(uids).anyMatch(mentionName::equalsIgnoreCase)) {
+                    return true;
+                }
+            }
+
+            //Check if the author of the original tweet is in the list
+            final String originalAuthor = get(object, String.class, "retweeted_status", "user", "screen_name");
+            if (originalAuthor != null && Arrays.stream(uids).anyMatch(originalAuthor::equalsIgnoreCase)) {
+                return true;
+            }
+
+            //No match was found
+            return false;
+        }
+
+        @Override
+        public void flatMap(String value, Collector<String> out) {
+            try {
+                JsonObject object = GSON.fromJson(value, JsonObject.class);
+
+                if (object == null) {
+                    return;
+                }
+
+                if (checkIfShouldPassThrough(object)) {
+                    out.collect(value);
+                }
+            } catch (final Throwable e) {
+                //Don't care much about thrown away records
+            }
+        }
+    }
+
+    private static CommandLine.Parser provideParameterList() {
+        return CommandLine.parser()
+                .withOption("t", TWEETS_PATH,
+                        "specifies the directory from which to get a stream of tweets", "DIRECTORY",
+                        CommandLine.Type.STRING, true, false, true)
+                .withOption("r", RESULTS_PATH,
+                        "specifies the directory to which the results will be saved", "DIRECTORY",
+                        CommandLine.Type.STRING, true, false, true)
+                .withOption("l", LIST_PATH,
+                        "specifies the file with the list of user handlers to filter", "FILE",
+                        CommandLine.Type.STRING, true, false, true);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            // Parse command line
+            final CommandLine cmd = provideParameterList().parse(args);
+
+            //noinspection ConstantConditions
+            final String tweetsPath = cmd.getOptionValue(TWEETS_PATH, String.class);
+            //noinspection ConstantConditions
+            final String resultsPath = cmd.getOptionValue(RESULTS_PATH, String.class);
+            //noinspection ConstantConditions
+            final String listPath = cmd.getOptionValue(LIST_PATH, String.class);
+
+            String[] uids = Files.asCharSource(new File(listPath), Charsets.UTF_8)
+                    .readLines().stream()
+                    .map(line -> line.split(",")[1])
+                    .toArray(String[]::new);
+            LOGGER.info(String.format("Loaded %d uids", uids.length));
+
+            FilterUserData extractor = new FilterUserData(uids);
+            extractor.startPipeline(tweetsPath, resultsPath);
+        } catch (final Throwable ex) {
+            // Handle exception
+            CommandLine.fail(ex);
+        }
+    }
+}
